0097bdb826a57c8d2b0c0138b12c9e0794ec2793
Richard Heise <richardfhf5@gmail.com>
2023-11-19 22:44:48 -0300

    Resultados ws

--- run_all.sh
+++ run_all.sh
@@ -6,28 +6,28 @@ run_test() {
     local cities="$2"
     local threads="$3"
 
-    echo "$cities cities with $threads" cores >> results_seq.st
-    (cd "$folder" && ./run.sh "Relevants/$cities" | grep "Tempo" | cut -d " " -f3) >> results_seq.st
+    echo "$cities cities with $threads" cores >> results_ws.st
+    (cd "$folder" && ./run.sh "$threads" "Relevants/$cities" | grep "Tempo" | cut -d " " -f3) >> results_ws.st
 }
 
-echo "Starting test round..." >> results_seq.st
+echo "Starting test round..." >> results_ws.st
 
 # Configurações sequenciais
-echo -e "\nSequential data:" >> results_seq.st
+# echo -e "\nSequential data:" >> results_ws.st
 
-for i in {16..18}; do
-    run_test "sequential" "$i" "1"
-done
+# for i in {16..18}; do
+#     run_test "sequential" "$i" "1"
+# done
 
 # Configurações paralelas com diferentes valores de OMP_NUM_THREADS
-#echo -e "\nParallel data:" >> results.st
+echo -e "\nParallel data:" >> results_ws.st
 
-#for threads in 2 3 4; do
-#    echo -e "\nTesting with OMP_NUM_THREADS=$threads\n" >> results.st
+for threads in 2 3 4; do
+    echo -e "\nTesting with $threads cores\n" >> results_ws.st
 
-#    for i in {16..18}; do
-#        run_test "parallel" "$i" "$threads"
-#    done
-#done
+    for i in {16..18}; do
+        run_test "parallel" "$i" "$threads"
+    done
+done
 
-echo "ALL DONE" >> results_seq.st
+echo "ALL DONE" >> results_ws.st
